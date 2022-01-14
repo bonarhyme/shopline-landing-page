7b2229aec3db18f7e3d6a8d3b6518445db104104
Bonaventure <Bonarhyme@gmail.com>
2022-01-14 20:35:31 +0100

    Added Navbar

--- components/MyNav.js
+++ components/MyNav.js
@@ -1,7 +1,54 @@
+import Image from "next/image";
 import React from "react";
+import { Container } from "react-bootstrap";
+import { appData } from "../variables/data";
 
 const MyNav = () => {
-  return <div>Nav</div>;
+  return (
+    <Container fluid>
+      <nav>
+        <div className="logo-container">
+          <Image
+            src={appData.logo}
+            alt="shopline logo"
+            width={150}
+            height={80}
+            objectFit="contain"
+          />
+        </div>
+        <div className="download-container">
+          <a
+            href="#"
+            className="nav-icon-container"
+            target="_blank"
+            rel="noopener noreferrer "
+          >
+            <Image
+              src={appData.google}
+              alt="Google Play"
+              width={150}
+              height={80}
+              objectFit="contain"
+            />
+          </a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer "
+            className="nav-icon-container"
+          >
+            <Image
+              src={appData.apple}
+              alt="Appstore"
+              width={110}
+              height={80}
+              objectFit="contain"
+            />
+          </a>
+        </div>
+      </nav>
+    </Container>
+  );
 };
 
 export default MyNav;
---
+++ variables/data.js
@@ -0,0 +1,9 @@
+import logo from "../public/logo.png";
+import apple from "../public/apple.png";
+import google from "../public/google.png";
+
+export const appData = {
+  logo,
+  apple,
+  google,
+};
