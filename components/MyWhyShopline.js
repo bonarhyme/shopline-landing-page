8f1dd0e949fadb0a4a34080a325c922c501e16b5
Bonaventure <Bonarhyme@gmail.com>
2022-01-19 15:53:29 +0100

    Added why choose shopline

--- components/MyWhyShopline.js
+++ components/MyWhyShopline.js
@@ -1,7 +1,62 @@
+import Link from "next/link";
 import React from "react";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { HiOutlineLightningBolt } from "react-icons/hi";
+import { FiWifiOff } from "react-icons/fi";
+import { BiGitBranch } from "react-icons/bi";
 
 const MyWhyShopline = () => {
-  return <div>My Why shopline</div>;
+  return (
+    <section className="why-shopline">
+      <h2 className="main-header">Why Shopline?</h2>
+      <div className="reasons-container">
+        <div>
+          <div>
+            <HiOutlineLightningBolt size={45} color="#5458f7" />
+          </div>
+          <p>Super-fast setup</p>
+          <p>
+            Create your beautiful shop in 2 minutes, No code, No website needed{" "}
+          </p>
+        </div>
+        <div>
+          <div>
+            <FiWifiOff size={45} color="#5458f7" />
+          </div>
+          <p>Save Mobile data</p>
+          <p>
+            Make sales even when you are offline. Receive orders by SMS. No
+            carrier charges applied
+          </p>
+        </div>
+        <div>
+          <div>
+            <BiGitBranch size={45} color="#5458f7" />
+          </div>
+          <p>Reach more customers</p>
+          <p>
+            Connect directly with customers anytime, wherever you are. No need
+            for physical store
+          </p>
+        </div>
+        <div>
+          <div className="money">
+            <div className="line"></div>
+            <FaMoneyBillAlt size={45} color="#5458f7" />
+          </div>
+          <p>No Commissions charged</p>
+          <p>
+            We don&apos;t take commissions off any sales you make. Rather, we
+            charge a monthly fee which gets smaller as your sales increase
+          </p>
+        </div>
+      </div>
+
+      <div className="auth-container">
+        <a href="#">Create Shop for Free</a>
+      </div>
+    </section>
+  );
 };
 
 export default MyWhyShopline;
